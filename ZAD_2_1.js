cbfbb6ec9d2165fbeffdc2f7f9a0573779869d13
Bern <bernard.kulegowski@gmail.com>
2019-10-26 13:23:40 +0200

    jeszcze trzy

--- ZAD_2_1.js
+++ ZAD_2_1.js
@@ -1,44 +1,4 @@
-// var adder = {
-//     base: 1,
-  
-//     add: function(a) {
-//       var f = v => v + this.base;
-//       return f(a);
-//     },
-  
-//     addThruCall: function(a) {
-//       var f = v => v + this.base;
-//       var b = {
-//         base: 2
-//       };
-  
-//       return f.call(b, a);
-//     }
-//   };
-  
-//   console.log(adder.add(1));         // This would log 2
-//   console.log(adder.addThruCall(1)); // This would log 2 still
 
-//let ar=[1,6,23,8,4,8,s,7];
-// var a=[1,6,23,8,4,8,6,7]
-// function sum(){
-//     var args = [].slice.call(arguments);
-//     return args.reduce(function(z,h){
-//       if(!isNaN(h)){
-//         return z+h;
-//       }
-//       else
-//       {
-//        throw console.log("Non-Numeric arguments"+h);
-//       }
-//    },0)
-//    }
-//        var result = sum(1,6,23,8,4,8,6,7); //pass any number of parameter to sum
-//        console.log("Sum Is:"+result);
-// console.log()
-
-// const array1 = [1, 2, 3, 4];
-// const reducer = (accumulator, currentValue) => accumulator + currentValue;
 //#1
 // var ar=[1,"s",23,8,4,7,"o",7];
 // const arFilltered=ar.filter(function(arVal){
@@ -55,15 +15,13 @@
 
 //#2
 // var array=[6,6,23,8,4,8,3,99];
-// //const first=aray[0], last=aray[aray.length-1];
-//  function arGay(a=[]){
+//  function arFirstLast(a=[]){
 //      first=a[0];
 //      last=a[a.length-1];
-// return first+last;
-//     }
-// //console.log(first+last);
+//      return first+last;
+// }
 
-// console.log(arGay(array));
+// console.log(arFirstLast(array));
 
 //#3
 
@@ -78,7 +36,7 @@
 //     }
 //     console.log(b);
 // });
- 
+
 //#4
 // let array = [7, 1, 2, 5, 10];
 
@@ -96,24 +54,16 @@
 
 // let array = [7, 1, 2, 5, 10, 11];
 // function par(dyn) {
-// //   let dyna = [],i=dyn.length;
 
-// //    while (i>0){function sUm(a,b){a+b [(dyn.splice(0,2))]};--i;}
-// // console.log(dyn);
-// // }
-
-// // console.log(par(array))
-// // console.log(array.splice(0,2),array)
 // let i=0,a=0,b=1,z=[];
+
 // while (i<=(dyn.length-1)/2){z.push(dyn[a]+dyn[b]);a=a+2, b=b+2, i++}
 //   return z;
  
 // }
 // console.log(par(array))
-// console.log(z)
 
-// #6
-
+//#6 sumting wrong nie tedy droga
 // let array = [7, 1, 2, 5, 10];
  
 // function srif(z)
@@ -123,7 +73,7 @@
 // {if (dyn.length%2 !==0 || a==dyn.length-1) {
 //   return push(dyn[a]+dyn[a]);}
 //     else {
-//       return push(dyn[a]+dyn[b]);}
+//     return push(dyn[a]+dyn[b]);}
 // }
 // function par(dyn) {
 
@@ -134,22 +84,22 @@
 //   return z;
  
 // }
-// console.log(par(array));
-//console.log(z);
+//  console.log(par(array));
+// console.log(z);
 
 //7
 
 // let array = [7, 1, 2, 5, 10, 111, 121, 23, 45, 77];
 
 // function getaRa(arA) {
-//   // let a=[];
+//   
 //   min=0;
 //   max=arA.length-1;
 //   return arA[Math.floor(Math.random()*(max-min+1))+min]
 // }
 // console.log(getaRa(array));
 
-//8
+//#8
 
 // let array = [7, 1, 2, 5, 10, 111, 121, 23, 45, 77];
 
@@ -163,22 +113,21 @@
 //       max=aRa.length-1;
 //       z.push(aRa[Math.floor(Math.random()*(max-min+1))+min]); i++;
 //     }
-//     //return z;
 //     console.log(Math.min(...z));
 //     return z;
 
 //   }
 
-// console.log(untillcOu(array,cOunter);
+// console.log(untillcOu(array,cOunter));
 
-//9 sumting wrong
+//9 sumting wrong 
 
 // let array = [7, 1, 2, 5, 10, 77];
 
 // function untillEnd(aRa) {
 //   end=1;
 //   i=0;
-//   z=[]
+//   z=[];
 //   while (end>0) {
 //       end=aRa.length-1;
 //       min=0;
@@ -192,20 +141,37 @@
 //   }
 //   console.log(untillEnd(array));
 
-//11
-
-let dniTyg = ["Niedziela", "Nieszczesliwnik", "Wt", "Połmetek", "Czw", "Piatunio", "Sobota"]
+// console.log(dfQ);
 
 
-function dzienTyg() {
-  var dzien = new Date();
-  var n = dniTyg[dzien.getDay()];
-  return n;
-}
+//#11
 
-// var Xmas95 = new Date('December 25, 1995 23:15:30');
-// var weekday = Xmas95.getDay();
-// var options = { weekday: 'long'};
-// console.log(new Intl.DateTimeFormat('en-PL', options));
+// let dniTyg = ["Niedziela", "Nieszczesliwnik", "Wt", "Połmetek", "Czw", "Piatunio", "Sobota"]
 
-console.log("Dzisiaj jest :"+dzienTyg());
+
+// function dzienTyg() {
+//   var dzien = new Date();
+//   var n = dniTyg[dzien.getDay()];
+//   return n;
+// }
+// console.log("Dzisiaj jest :"+dzienTyg(dniTyg))
+// #12
+
+
+
+// #13
+
+// let zmiA=3;
+// let zmiB=4;
+
+// function matematyka(a,b) {
+//   var operacje={dodawanie:   a+b,
+//                 odejmowanie: a-b,
+//                 mnożenie:    a*b,
+//                 dzielenie:   a/b,};
+// return operacje ;               
+// }
+
+
+// console.log(matematyka(zmiA,zmiB));
+
